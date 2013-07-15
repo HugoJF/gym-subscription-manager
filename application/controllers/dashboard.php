4bb49d19c94b193f54c2b6d8b15512de1402107d
HugoJF <hugojellerferreira@hotmail.com>
2013-07-15 10:00:47 -0400

    removed all debugging functions

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -58,17 +58,6 @@
 			$this->load->view('dashboard_view', array('users' => $users));
 			$this->load->view('footer_view');
 		}
-
-		public function superpopulatepayments() {
-			$this->output->enable_profiler(TRUE);
-			for($i = 0; $i < 10000; $i ++) {
-				$data = array('ip_address' => '00000000000000000000000000000001', 'username' => 'administrator' . $i,
-							  'password'   => '3531e4e40812dd5ae8cb64a14e504c0b0ae71272',
-							  'email'      => 'admimnistrator' . $i . '@email.com', 'created_on' => '1373205992',
-							  'active'     => '1', 'first_name' => 'admin', 'last_name' => 'istrator',);
-				$this->db->insert('users', $data);
-			}
-		}
 		
 		public function csv_decryption() {
 			$data = $this->load->view('csv_data', '', TRUE);
@@ -81,59 +70,5 @@
 				if($data_s_proc[0] == '' || $data_s_proc[1] == '') continue;
 				array_push($data_processed, $data_s_proc);
 			}
-			
-			$final = array();
-			foreach($data_processed as $p) {
-				$rid = $p[0];
-				$name = $p[1];
-				$type = $p[2];
-				$bd = $p[4];
-				$gender = $p[5];
-				list($day, $month, $year) = explode('/', $bd);
-				$day = intval($day);
-				$month = intval($month);
-				$year = intval($year);
-				$bd = mktime(0, 0, 0, $month, $day, $year);
-				array_push($final, array(
-					'rid' => $rid,
-					'name' => $name,
-					'type' => $type,
-					'birthday' => date('d.m.Y', $bd),
-					'gender' => $gender
-				));
-			}
-			print_r($final);
-			$this->output->enable_profiler(true);
-		}
-		
-		function populate_payments() {
-		while(true) {
-			$data = array(
-				'user_id' => rand(0, 50),
-				'date' => date("Y-m-d H:i:s", time()),
-				'valid_until' => date("Y-m-d H:i:s", time())
-			);
-		$this->db->insert('payments', $data);
-		}
-		}
-		function populate_users() {
-			$i = 0;
-			while(true) {
-				$data = array(
-					'username' => 'administrator' . $i,
-					'password' => '59beecdf7fc966e2f17fd8f65a4a9aeb09d4a3d4',
-					'salt' => '9462e8eee0',
-					'email' => 'admin' . $i . '@admin.com',
-					'created_on' => 1268889823,
-					'last_login' => 1268889823,
-					'active' => 1,
-					'first_name' => 'admin' . $i,
-					'last_name' => 'istrator',
-					'company' => 'ADMIN'
-				);
-				$this->db->insert('users', $data);
-				$i++;
-			}
-		}
 		//http://www.youtube.com/watch?v=DlvM68JBYkQ
 	}
